4640dd4b35472c120458cdb9f193c8588a467999
EbBlovery <18217052735@163.com>
2017-08-25 22:24:23 +0800

    死bug

--- src/views/rank/index.vue
+++ src/views/rank/index.vue
@@ -1,22 +1,35 @@
 <template>
 	<div>
 		<template class="boys">
-		    <p>男生</p>
-		    <el-row :key="index" v-if="!item.collapse" v-for="(item,index) in list.male">
+		    <p class="header">男生</p>
+		    <el-row class="rank-list" :key="index" v-if="!item.collapse" v-for="(item,index) in list.male">
 		    	<el-col :span="4"><img :src="'http://statics.zhuishushenqi.com' + item.cover"></el-col>
-		    	<el-col :span="20">{{item.title}}</el-col>
+		    	<el-col :span="20"><p>{{item.title}}</p></el-col>
+		    </el-row>
+		    <el-row class="rank-list" @click.native="handleToggle()">
+		    	<el-col :span="4">阿萨德</el-col>
+		    	<el-col :span="20"><p>别人家的排行榜</p></el-col>
+		    </el-row>
+		    <el-row class="rank-list" :class="{isshow:show1}" :key="index" v-if="item.collapse" v-for="(item,index) in list.male">
+		    	<el-col :span="4"><img style="opacity:0" src="#"></el-col>
+		    	<el-col :span="20"><p>{{item.title}}</p></el-col>
 		    </el-row>
 		</template>
-		<!-- <div>
-			{{maleList}}
-		</div> -->
-		<!-- <template class="girls">
-			<p>女生</p>
-			<el-row v-for="item in listFemale">
+		<template class="boys">
+		    <p class="header">女生</p>
+		    <el-row class="rank-list" :key="index" v-if="!item.collapse" v-for="(item,index) in list.female">
 		    	<el-col :span="4"><img :src="'http://statics.zhuishushenqi.com' + item.cover"></el-col>
-		    	<el-col :span="20">{{item.title}}</el-col>
+		    	<el-col :span="20"><p>{{item.title}}</p></el-col>
 		    </el-row>
-		</template> -->
+		    <el-row class="rank-list" @click.native="handleToggle2()">
+		    	<el-col :span="4">阿萨德</el-col>
+		    	<el-col :span="20"><p>别人家的排行榜</p></el-col>
+		    </el-row>
+		    <el-row class="rank-list" :class="{isshow:show2}" :key="index" v-if="item.collapse" v-for="(item,index) in list.female">
+		    	<el-col :span="4"><img style="opacity:0" src="#"></el-col>
+		    	<el-col :span="20"><p>{{item.title}}</p></el-col>
+		    </el-row>
+		</template>
 	</div>
 </template>
 <script>
@@ -24,7 +37,9 @@ import axios from 'axios'
 export default {
 	data(){
 		return {
-            list: []
+            list: [],
+            show1:false,
+            show2:false
 		}
 	},
 	beforeRouteEnter(to,from,next){
@@ -39,6 +54,12 @@ export default {
              axios.get(`/ranking/gender`).then(res=>{
                 this.list = res.data
              })
+		},
+		handleToggle(){
+			this.show1 = !this.show1
+		},
+		handleToggle2(){
+			this.show2 = !this.show2
 		}
 	},
 	computed: {
@@ -46,3 +67,31 @@ export default {
 	}
 }
 </script>
+
+<style lang="less" scoped>
+.isshow{
+	display:none;
+}
+.header{
+	width:100%;
+	height:3.5rem;
+	background:rgb(230,230,230);
+	line-height:3.5rem;
+	text-align:left;
+	text-indent:1rem;
+	color:rgb(153,153,153);
+	font-size:.8rem;
+}
+.rank-list{
+	margin: 1.8rem 0;
+	img{
+		width:2.5rem;
+		height:2.5rem;
+	}
+	p{
+		text-align:left;
+		height:2.5rem;
+		line-height:2.5rem;
+	}
+}
+</style>
