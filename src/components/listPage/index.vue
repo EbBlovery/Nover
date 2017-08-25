8e430b7fe1611dac3d89a1094559125844321d1e
EbBlovery <18217052735@163.com>
2017-08-25 16:38:41 +0800

    分类

--- src/components/listPage/index.vue
+++ src/components/listPage/index.vue
@@ -1,6 +1,33 @@
 <template>
 	<div>
-		list
+		<template class="boys">
+		    <p class="header-bar">男生</p>
+			<el-row class="header-info">
+				<el-col :span="8" class="header-col" :key='index' v-for="(item,index) in list.male">
+					<p>{{item.name}}</p>
+					<span>{{item.bookCount}}</span>
+				</el-col>
+			</el-row>
+		</template>
+		<template class="girls">
+		    <p class="header-bar">女生</p>
+		    <el-row class="header-info">
+		    	<el-col :span="8"  class="header-col" :key='index' v-for="(item,index) in list.female">
+					<p>{{item.name}}</p>
+					<span>{{item.bookCount}}</span>
+				</el-col>
+		    </el-row>
+		</template>
+		<template class="press">
+		    <p class="header-bar">出版</p>
+		    <el-row class="header-info">
+		    	<el-col class="header-col" :span="8" :key='index' v-for="(item,index) in list.press">
+					<p>{{item.name}}</p>
+					<span>{{item.bookCount}}</span>
+				</el-col>
+		    </el-row>
+			
+		</template>
 	</div>
 </template>
 <script>
@@ -8,13 +35,13 @@ import axios from 'axios'
 export default {
 	data(){
 		return {
-
+            list: []
 		}
 	},
 	methods: {
         get(){
             axios.get(`/cats/lv2/statistics`).then(res=>{
-            	console.log(res)
+            	this.list = res.data
             })
         }
 	},
@@ -27,3 +54,28 @@ export default {
 	}
 }
 </script>
+
+<style lang="less" scoped>
+.header-bar{
+	color:#5c5d58;
+	text-align:left;
+	margin: 1rem;
+	font-size:.9rem;
+}
+.header-info{
+	.header-col{
+		margin:.7rem 0;
+		p{
+	    	color:#000;
+	    	font-size:1rem;
+	    	font-weight:bold;
+	    	margin:.2rem 0;
+	    }
+	    span{
+	    	color:#5c5d58;
+	    	font-size:.8rem;
+	    	opacity:.5;
+	    }
+	}
+}
+</style>
